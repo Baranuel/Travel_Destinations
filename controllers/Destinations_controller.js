8d759b672214b4ddb35ab814b4419b1c415be40a
Baranuel <samuel.baran98@gmail.com>
2022-10-11 13:15:59 +0200

    controllers

---
+++ controllers/Destinations_controller.js
@@ -0,0 +1,16 @@
+const getDestinations = async (req, res) => {
+  //we declare an asynchronous function so we can await changes from database
+  try {
+    //we create our query, assign it to a variable and await it
+    // const destinations = await client
+    //   .db("travel_destinations")
+    //   .collection("destinations")
+    //   .find()
+    //   .toArray();
+
+    const allDestinations = await Destination.find();
+    res.status(200).json(allDestinations);
+  } catch (err) {
+    console.log(err);
+  }
+};
