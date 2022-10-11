0f2c4ad75b619a8e84e8a992fac518ef9387441c
Martin Windsor <martinwindsor1@gmail.com>
2022-10-11 12:58:26 +0200

    First draft of the user schema
    Created first draft of the user schema

---
+++ user-schema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const user_schema = new mongoose.Schema({
+  userEmail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", user_schema, "users");
+
+export default User;
