377dbd2170544d4287e704a40bacbe09e52ff4ca
DentistLeeroy <74152951+DentistLeeroy@users.noreply.github.com>
2022-10-13 12:04:33 +0200

    Revert "no idea what I'm doing"
    This reverts commit 652c595f598d8c32e468eb2078cf3b1ce367bc43.

--- controllers/User_controller.js
+++ controllers/User_controller.js
@@ -16,16 +16,21 @@ export const createUser = async (req, res) => {
 export const getUser = async (req, res) => {
   const payload = req.body;
   console.log(payload);
+  try {
     const user = await User.findOne({
       userEmail: payload.userEmail,
       userPassword: payload.userPassword,
     });
     console.log(user);
     const token = createToken(user);
-    res.redirect("https://youtu.be/dQw4w9WgXcQ");
-    // res.cookie("token", token, {
-    //     httpOnly: true,
-    //     sameSite: "strict"
-    //   })
-    //   .json(`Log in successful. Hello ${user.userEmail}. We are sending your data and password (${user.userPassword}) to the FBI.`);
+
+    res.
+      cookie("token", token, {
+        maxAge: 60 * 60 * 24 * 30 * 1000,
+        httpOnly: true,
+      })
+      .json(`successfully logged in, Hello ${user.firstName}`);
+  } catch (error) {
+    console.log(error);
+  }
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //all the imports 
  import express from 'express'
  import { MongoClient, ObjectId } from 'mongodb'
-// import cors from 'cors'
+import cors from 'cors'
 import Travel from "./models/travel-scheme.js"
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
@@ -10,54 +10,48 @@ import mongoose from 'mongoose'
 //importing controllers
 import { createUser, getUser } from "./controllers/User_controller.js"
 
-// const corsOptions ={
-//     origin:'*', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200,
-// }
-
-// database setup
-const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ohvc7rg.mongodb.net/mongoose_travel_destinations"
+ // database setup
+ const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ohvc7rg.mongodb.net/mongoose_travel_destinations"
 //  const client = new MongoClient(uri)
 mongoose.connect(uri);
 
-//server setup
-const app = express();
-const port = 3000;
+ //server setup
+ const app = express();
+ const port = 3000;
 
-//server configuration so our requests accept JSON format
+ //server configuration so our requests accept JSON format
 app.use(express.json())
-// app.use(cors(corsOptions))
+app.use(cors())
 
 // API routes 
 app.get('/', async (req, res) => {
     //we declare an asynchronous function so we can await changes from database
-    try {
-        //we open a connection to database
-        await client.connect()
-        //we create our query, assign it to a variable and await it
-        const destinations = await client.db("travel_destinations").collection("destinations").find().toArray()
-        console.log(destinations)
+        try {
+            //we open a connection to database
+           await client.connect()
+            //we create our query, assign it to a variable and await it
+           const destinations = await client.db("travel_destinations").collection("destinations").find().toArray()
+           console.log(destinations)
         res.send(destinations)
         res.status(201)
-        
-    }
-    catch(err) {
-        console.log(err)
-    }
-    finally{
-        await client.close();
-    }
+         
+        }
+        catch(err) {
+            console.log(err)
+        }
+        finally{
+            await client.close()
+        }
 })
 app.post("/", async(req, res) => {
         //we declare an asynchronous function so we can await changes from database
         try {
             //we open a connection to database
-            await client.connect()
+           await client.connect()
             //we create our query, assign it to a variable and await it
-            const destinations = await client.db("travel_destinations").collection("destinations").insertOne(req.body)
-            res.send(destinations)
-            
+           const destinations = await client.db("travel_destinations").collection("destinations").insertOne(req.body)
+        res.send(destinations)
+         
         }
         catch(err) {
             console.log(err)
@@ -65,51 +59,51 @@ app.post("/", async(req, res) => {
         finally {
             client.close()
         }
-    })
-    
-    app.post("/auth/signup", createUser)
-    app.post("/auth/login", getUser)
-    
-    
-    app.put("/destination/:id", async(req, res) => {
-        //we declare an asynchronous function so we can await changes from database
-        try {
-            //we open a connection to database
-            await client.connect()
-            const paramsId = req.params.id
-            const updatedDestination = req.body
-            console.log(updatedDestination)
-            console.log(paramsId)
-            //we create our query, assign it to a variable and await it
-            const destination = await client.db("travel_destinations").collection("destinations").updateOne({_id:ObjectId(paramsId)}, {$set:updatedDestination})
-            res.send(destination)
-            
-        }
-        catch(err) {
-            console.log(err)
-        }
-        finally {
-            client.close()
-        }
-    })
-app.get('/destination/:id', async (req, res) => {
+})
+
+app.post("/auth/signup", createUser)
+app.post("/auth/login", getUser)
+
+
+app.put("/destination/:id", async(req, res) => {
     //we declare an asynchronous function so we can await changes from database
     try {
         //we open a connection to database
-        await client.connect()
+       await client.connect()
+       const paramsId = req.params.id
+       const updatedDestination = req.body
+       console.log(updatedDestination)
+       console.log(paramsId)
         //we create our query, assign it to a variable and await it
-        const destinations = await client.db("travel_destinations").collection("destinations").find({_id:ObjectId(req.params.id)}).toArray()
-        console.log(destinations)
-        res.json(destinations)
-        res.status(201)
-        
+       const destination = await client.db("travel_destinations").collection("destinations").updateOne({_id:ObjectId(paramsId)}, {$set:updatedDestination})
+    res.send(destination)
+     
     }
     catch(err) {
         console.log(err)
     }
-    finally{
-        await client.close()
+    finally {
+        client.close()
     }
+})
+app.get('/destination/:id', async (req, res) => {
+    //we declare an asynchronous function so we can await changes from database
+        try {
+            //we open a connection to database
+           await client.connect()
+            //we create our query, assign it to a variable and await it
+           const destinations = await client.db("travel_destinations").collection("destinations").find({_id:ObjectId(req.params.id)}).toArray()
+           console.log(destinations)
+        res.json(destinations)
+        res.status(201)
+         
+        }
+        catch(err) {
+            console.log(err)
+        }
+        finally{
+            await client.close()
+        }
 })
 
 app.listen(port, ()=> {
